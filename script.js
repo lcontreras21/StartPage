d70918c3e3060f651e2ff59add68bf9eb7d0e957
lcontreras21 <luiscontrerasorendain@gmail.com>
2024-09-15 11:27:41 -0400

    WIP

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function clock() {
 	var date = new Date();
 	var hours = ("0" + date.getHours()).slice(-2);
 	var minutes = ("0" + date.getMinutes()).slice(-2);
-    $('#clock').html(hours + ':' + minutes);
+    $('.clock p').html(hours + ':' + minutes);
 	setTimeout(clock, 500);
 }
 clock()
 
-// Prevent Email dropdown from closing
-$('.Emails .dropdown-menu').on('click', function (e) {
+// Prevent dropdown from closing
+// TODO make this a tag of some sort assignable per dropdown
+$('.dropdown-menu').on('click', function (e) {
     e.stopPropagation();
 });
