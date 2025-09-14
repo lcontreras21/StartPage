e25a945f526a98427223a93b1030b6ad5f956343
lcontreras21 <luiscontrerasorendain@gmail.com>
2025-09-14 15:51:27 +0000

    filename fix

--- autoconfig.js
+++ autoconfig.js
@@ -1,4 +1,4 @@
 //  
-pref("general.config.filename", "autoconfig.cfg");
+pref("general.config.filename", "UserChrome.js");
 pref("general.config.obscure_value", 0);
 pref("general.config.sandbox_enabled", false);
